c7845ab6d998d588001c6068b7a1d69593d01c8d
Nikodem Korohoda <nikodemko@interia.pl>
2024-03-05 12:49:40 +0100

    basic map generated

--- scripts/path_generator.gd
+++ scripts/path_generator.gd
@@ -10,15 +10,15 @@ func _init(length: int, height: int):
 	_grid_length = length
 	_grid_height = height
 
-func valid_cords(x: int, y: int):
-	if _path.has(Vector2i(x, y)) or\
-		_path.has(Vector2i(x-1, y)) and _path.has(Vector2i(x+1, y)) or\
-		_path.has(Vector2i(x-1, y)) and _path.has(Vector2i(x, y-1)) or\
-		_path.has(Vector2i(x-1, y)) and _path.has(Vector2i(x, y+1)) or\
-		_path.has(Vector2i(x+1, y)) and _path.has(Vector2i(x, y-1)) or\
-		_path.has(Vector2i(x+1, y)) and _path.has(Vector2i(x+1, y+1)) or\
-		_path.has(Vector2i(x, y-1)) and _path.has(Vector2i(x, y+1)):
+func is_valid_coords(x: int, y: int) -> bool:
+	if x <= 0 or x > _grid_length or y <= -_grid_height or y >= _grid_height:
 		return false
+	
+	var sides = [Vector2i(x-1, y), Vector2i(x+1, y), Vector2i(x, y-1), Vector2i(x, y+1)]
+	for s1 in sides:
+		for s2 in sides:
+			if s1!=s2 and _path.has(s1) and _path.has(s2):
+				return false
 	return true
 
 func generate_path():
@@ -27,34 +27,33 @@ func generate_path():
 	var x = 0
 	var y = 0
 	
-	var couter = 0
-	
-	_path.append(Vector2i(0, 0))
-	
-	while x < _grid_length:
-		if couter > 10000:
-			x = 0
-			y = 0
-			_path.clear()
-		
-		var choice: int = randi_range(0,4)
-		var x_old = x
-		var y_old = y
-		
-		if choice == 0 and x < _grid_length:
-			x += 1
-		elif choice == 1 and x > 0:
-			x -= 1
-		elif choice == 2 and y < _grid_height:
-			y += 1
-		elif choice == 3 and y > -_grid_height:
-			y -= 1
-		
-		if valid_cords(x, y):
+	while x <= _grid_length:
+		if not _path.has(Vector2i(x, y)):
 			_path.append(Vector2i(x, y))
-		else:
-			x = x_old
-			y = y_old
-		couter += 1
+		
+		var choice: int = randi_range(0, 2)
+		
+		if choice == 0 or x % 2 == 0 or x == _grid_length-1:
+			x += 1
+		elif choice == 1 and y < _grid_height-1 and not _path.has(Vector2i(x, y+1)):
+			y += 1
+		elif choice == 2 and y > -_grid_height+1 and not _path.has(Vector2i(x, y-1)):
+			y -= 1
+			
 		
 	return _path
+
+func get_tile_score(tile: Vector2i):
+	var score: int = 0
+	var x = tile.x
+	var y = tile.y
+	
+	score += 1 if _path.has(Vector2i(x, y-1)) else 0
+	score += 2 if _path.has(Vector2i(x+1, y)) else 0
+	score += 4 if _path.has(Vector2i(x, y+1)) else 0
+	score += 8 if _path.has(Vector2i(x-1, y)) else 0
+	
+	return score
+
+
+
